19b1cd9b4878f50e3cb346d46c24d6ccdb210347
Thayanne Luiza <thayannevls@gmail.com>
2014-03-06 22:20:31 -0300

    Introdução a funções e Arquivos à solta e atulização de exercícios

---
+++ Funções e arquivos à solta/Teste1.py
@@ -0,0 +1,7 @@
+texto = 'aeiou'
+contador = 0
+while contador < len(texto):
+	letra = texto[contador]
+	print(letra)
+	contador += 1
+	
--- Lista-3 Zumbis/Fibonacci.py
+++ Lista-3 Zumbis/Fibonacci.py
@@ -1,7 +1,7 @@
-n = int(input('Digite um número:'))
-a,b = 1,1
-k = 1
-while k < n-2:
-	a,b = b, a + b
-	k += 1
+n = int(input('Digite um número:')) #posicao do numero fibonacci
+a,b = 1,1 #primeiros numeros da sequencia
+contador = 1
+while k < n - 2: #condicao
+	a,b = b, a + b #somando com o anteiror
+	k += 1 #adicionando mais um ao contador
 print('F(%d): %d' %(n,b))
