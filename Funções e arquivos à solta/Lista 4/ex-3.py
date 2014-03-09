9ffd39aea2f2927bdb5d58828dfbc75756d3c197
Thayanne Luiza <thayannevls@gmail.com>
2014-03-09 17:50:02 -0300

    ex-4

--- Funções e arquivos à solta/Lista 4/ex-3.py
+++ Funções e arquivos à solta/Lista 4/ex-3.py
@@ -3,4 +3,10 @@ vetor1 = []
 vetor2 = []
 vetor3 = []
 for i in range(10):
-	vetor.append(random.randint
+	vetor1.append(random.randint(1,100))
+	vetor2.append(random.randint(1,100))
+	vetor3.append(vetor1[i])
+	vetor3.append(vetor2[i])
+print(vetor1)
+print(vetor2)
+print(vetor3)
